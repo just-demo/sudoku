0e207b0e88648d86f47b7c88e22d54f2c52a6f6f
Anatolii Kryvda <anatolii.kryvda@automationanywhere.com>
2017-12-13 18:47:02 +0200

    More data

--- src/main/java/self/ed/SudokuGenerator.java
+++ src/main/java/self/ed/SudokuGenerator.java
@@ -10,6 +10,7 @@ import java.util.*;
 import static java.util.Collections.*;
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 import static java.util.stream.IntStream.rangeClosed;
 import static self.ed.SudokuUtils.copy;
 import static self.ed.SudokuUtils.countOpen;
@@ -17,10 +18,12 @@ import static self.ed.SudokuUtils.countOpen;
 public class SudokuGenerator {
     private int size;
     private int blockSize;
+    private Set<Integer> values;
 
     public SudokuGenerator(int size) {
         this.size = size;
         this.blockSize = (int) Math.sqrt(size);
+        this.values = rangeClosed(1, size).boxed().collect(toSet());
     }
 
     public Integer[][] generate() {
@@ -30,6 +33,59 @@ public class SudokuGenerator {
     public Integer[][] generate(int complexityLowerLimit) {
         return generate(new Integer[size][size], complexityLowerLimit);
     }
+
+//    public Integer[][] minimizeMax(Integer[][] initialValues, Collection<Cell> closeCandidates) {
+//        Set<Integer> defaultValues = rangeClosed(1, size).boxed().collect(toSet());
+//
+//        result = new Integer[size][size];
+//        List<Cell> open = new ArrayList<>();
+//        for (int row = 0; row < size; row++) {
+//            for (int col = 0; col < size; col++) {
+//                int block = blockSize * (row / blockSize) + col / blockSize;
+//                Integer value = initialValues[row][col];
+//                if (value != null) {
+//                    open.add(new Cell(row, col, block, singleton(value)));
+//                } else {
+//                    pending.add(new Cell(row, col, block, defaultValues));
+//                }
+//            }
+//        }
+//
+//        open.forEach(this::open);
+//
+//        List<Cell> open = new ArrayList<>();
+//        for (int row = 0; row < size; row++) {
+//            for (int col = 0; col < size; col++) {
+//                if (initialValues[row][col] != null) {
+//                    open.add(new Cell(row, col, 0, emptySet()));
+//                }
+//            }
+//        }
+//
+//        Map<Cell, Integer[][]> candidates = new HashMap<>();
+//        closeCandidates.forEach(cell -> {
+//            Integer[][] nextGuess = copy(initialValues);
+//            nextGuess[cell.getRow()][cell.getCol()] = null;
+//            try {
+//                new SudokuSolver(nextGuess).solve();
+//                candidates.put(cell, nextGuess);
+//            } catch (MultipleSolutionsException e) {
+//                // no-op
+//            }
+//        });
+//
+//        List<Cell> nextCloseCandidates = new ArrayList<>(candidates.keySet());
+//        shuffle(nextCloseCandidates);
+//        return new ArrayList<>(nextCloseCandidates).stream()
+//                .map(cell -> {
+//                    nextCloseCandidates.remove(cell);
+//                    return minimize(candidates.get(cell), nextCloseCandidates);
+//                })
+//                .map(matrix -> Pair.of(matrix, countOpen(matrix)))
+//                .min(comparing(Pair::getValue))
+//                .map(Pair::getKey)
+//                .orElse(initialValues);
+//    }
 
     public Integer[][] minimize(Integer[][] initialValues) {
         List<Cell> open = new ArrayList<>();
@@ -96,7 +152,7 @@ public class SudokuGenerator {
             shuffle(pending);
             for (Cell cell : pending) {
                 Integer[][] nextGuess = copy(initialValues);
-                List<Integer> values = rangeClosed(1, size).boxed().collect(toList());
+                List<Integer> values = new ArrayList<>(this.values);
                 open.stream().filter(cell::related).map(Cell::getValue).forEach(values::remove);
                 shuffle(values);
                 for (Integer value : values) {
